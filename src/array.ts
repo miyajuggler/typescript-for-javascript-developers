2b38e5111ec8dc06d10c78172267ed2a5ca40f0e
miyajuggler <b.m.juggler@gmail.com>
2021-06-07 22:24:29 +0900

    src/any.ts

---
+++ src/array.ts
@@ -0,0 +1,18 @@
+export { };
+
+
+let numbers: number[] = [1, 2, 3];
+console.log(numbers);
+
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings: string[] = ["typescript", "Javascript", "Coffeescript"];
+let strings2: Array<string> = ["typescript", "Javascript", "Coffeescript"];
+
+
+let nijigenhairetsu: number[][] = [
+  [50, 100],
+  [500, 100]
+];
+
+let hairetu: (string | number | boolean)[] = [1, false, "japan"];
---
+++ src/number-string.ts
@@ -0,0 +1,16 @@
+export {};
+
+let year: number = 1976;
+// year = true;
+console.log(year);
+
+
+let age: number = 0x2b;
+
+console.log(age);
+
+
+
+let name: string = "miyazaki";
+// name = 3;
+console.log(name);
